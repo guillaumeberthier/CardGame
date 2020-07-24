9452755cef00e1c8734e2e82af6c0a495efe8fc4
Guillaume Berthier <berthier.gllm@gmail.com>
2020-07-24 14:53:02 +0200

    Add ResolutionResult on stage

--- Sources/CardGame/Stage.swift
+++ Sources/CardGame/Stage.swift
@@ -7,20 +7,42 @@
 
 import Foundation
 
+public enum ResolutionResult {
+    case authorized
+    case unauthorized
+}
+
 public protocol Stage {
     var name: String { get }
 
     func execute()
     func player(for stage: Stage) -> Player?
     func playerAction(from stage: Stage, player: Player) -> PlayerAction?
-    func resolve(_ action: PlayerAction,from player: Player, in stage: Stage)
+    func resolve(_ action: PlayerAction, from player: Player, in stage: Stage) -> ResolutionResult
 }
 
 extension Stage {
     public func execute() {
         while let player = player(for: self) {
-            guard let action = playerAction(from: self, player: player) else { continue }
-            resolve(action, from: player, in: self)
+            let _ = getValidResolution(player: player)
         }
+    }
+
+    // MARK: - Private
+
+    private func getValidResolution(player: Player) -> ResolutionResult {
+        let action = getValidAction(from: player)
+        let result = resolve(action, from: player, in: self)
+        guard result == .authorized else {
+            return getValidResolution(player: player)
+        }
+        return result
+    }
+
+    private func getValidAction(from player: Player) -> PlayerAction {
+        guard let action = playerAction(from: self, player: player) else {
+            return getValidAction(from: player)
+        }
+        return action
     }
 }
